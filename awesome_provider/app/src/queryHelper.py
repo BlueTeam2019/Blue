3371372e586c8c24e07ee548f533843964eaa54b
Efi <efishimon@gmail.com>
2019-12-22 19:13:00 +0200

    On branch feature/sqlHelper - removed unnecessary print() from code  Changes to be committed: 	modified:   queryHelper.py

--- awesome_provider/app/src/queryHelper.py
+++ awesome_provider/app/src/queryHelper.py
@@ -27,9 +27,6 @@ class QueryHelper():
                            
         cur = db.cursor()
         cur.execute(""" select 1  """)
-        data = cur.fetchall()       
-        print(data)
-        print(data[0])
-        print(data[0][0])
+        data = cur.fetchall()               
         db.close()
-        return data[0][1]
+        return data[0][0]
