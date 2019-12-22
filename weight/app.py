f0d718a51eb754695c20dcdd3f2a758068fbe92f
shw3bs <liad@devops.com>
2019-12-22 18:45:31 +0200

    Add app index html

--- weight/app.py
+++ weight/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello Nuclear Geeks"
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
