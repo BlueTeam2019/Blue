678a1b0e8f7802a2c6ee7a5d09580a3e5c507918
Efi <efishimon@gmail.com>
2019-12-23 11:12:18 +0200

    refactor to sqlHelper code

--- awesome_provider/app/src/queryHelper.py
+++ awesome_provider/app/src/queryHelper.py
@@ -2,31 +2,40 @@ import MySQLdb
 
 
 class QueryHelper():
-    
-    
-    def GetConnection():
-        return MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
-                        user="root",         # your username
-                        passwd="secret",  # your password
-                        db="mysql",
-                        port=3306)        # name of the data base
-           
-
    # def __init__(self , url, usr, password, dbName):
    #     db = MySQLdb.connect(host="localhost",    # your host, usually localhost
    #                     user="root",         # your username
    #                     passwd="1",  # your password
-   #                     db="mysql")        # name of the data base
+   #                     db="mysql")        # name of the data base 
+    
+    def GetConnection():
+        return MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
+                        user="root",                # your username
+                        passwd="secret",            # your password
+                        db="mysql",                 # name of the data base
+                        port=3306)                  # port of the data base 
+           
+
+   
         
     def selectOne():
-        db = MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
-                        user="root",         # your username
-                        passwd="secret",  # your password
-                        db="mysql",
-                        port=3306)        # name of the data base                  
-                           
-        cur = db.cursor()
-        cur.execute(""" select 1  """)
-        data = cur.fetchall()               
-        db.close()
-        return data[0][0]
+        value = 0
+        try:
+            db = MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
+                        user="root",              # your username
+                        passwd="secret",          # your password
+                        db="mysql",               # name of the data base
+                        port=3306)                # port of the data base    
+            cur = db.cursor()
+            cur.execute(""" select 1  """)
+            data = cur.fetchall()       
+            value = data[0][0]
+            print(value)
+        
+        except:
+            print("Error in function: selectOne() please check")
+        finally:
+            db.close()
+                    
+        return int(value)
+
