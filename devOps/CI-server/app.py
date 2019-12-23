f97d15bf0aeb9792e15d4396789a630562f86045
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-23 16:15:48 +0200

    CI - adding build capability

--- devOps/CI-server/app.py
+++ devOps/CI-server/app.py
@@ -1,12 +1,18 @@
 import os
+import subprocess
+
+import yaml
 from flask import Flask, request
 from git import Repo
-import json
 
 app = Flask(__name__)  # Standard Flask app
 
 git_url = "git@github.com:BlueTeam2019/Blue.git"
 repo_dir = "/home/ubuntu/testing/"
+weight_path = "/weight/docker-compose.yam"
+providor_path = "/awesome_provider/docker-compose.yam"
+weight_path_prod = "/weight/docker-compose.yam"
+providor_path_prod = "/awesome_provider/docker-compose.yam"
 
 
 @app.route("/", )  # Standard Flask endpoint
@@ -23,7 +29,16 @@ def webhook():
     branch_name = os.path.basename(content["ref"])
 
     print("Got push with: {0},{1},{2},{3}".format(pusher, pusher_email, head_commit, branch_name))
-    create_repo_of_commit(git_url, repo_dir + head_commit, head_commit)
+    repo = repo_dir + head_commit
+    create_repo_of_commit(git_url, repo, head_commit)
+
+    build(repo + weight_path, repo + providor_path)
+    test_result = exec_tests()
+    send_report(test_result)
+    clear_test(head_commit)
+    if branch_name == "maaaster":
+        build(repo + weight_path_prod, repo + providor_path_prod)
+
     return "CI server webhooked".format(content)
 
 
@@ -32,7 +47,34 @@ def create_repo_of_commit(git_url, repo_dir, commit_hash):
     repo.git.checkout(commit_hash)
 
 
+def exec_tests():
+    return True
+
+
+def send_report(report):
+    return True
+
+
+def clear_test(commit):
+    return True
+
+
+def build(weight_path, provider_path):
+    subprocess.run("docker-compose -f {0} up --build -d".format(weight_path))
+    subprocess.run("docker-compose -f {0} up --build -d".format(provider_path))
+
+
+def edit_docker_compose_test(path):
+    with open(path) as f:
+        list_doc = yaml.load(f)
+
+    for sense in list_doc:
+        if sense["name"] == "sense2":
+            sense["value"] = 1234
+
+    with open("my_file.yaml", "w") as f:
+        yaml.dump(list_doc, f)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
-#create_repo_of_commit(git_url, "/home/itamar/ubuntu/testing", "50f09c64ffb57614a1526bba3a6382148670c020")
