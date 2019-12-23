96acf797036af2ca6cdb1cb5be95e9af1e96f9d6
Efi <efishimon@gmail.com>
2019-12-23 20:02:18 +0200

    added GetData and AlterData methods to queryHelper.py 	modified:   queryHelper.py

--- awesome_provider/app/src/queryHelper.py
+++ awesome_provider/app/src/queryHelper.py
@@ -1,41 +1,75 @@
-import MySQLdb
+import pymysql
+import sys
+import prettytable
 class QueryHelper():            
     #def __init__(self , url, usr, password, dbName, port):
     #    self.url = url
     #    self.usr = usr
     #    self.password = password
     #    self.dbName=dbName
-    #    self.port       
-   
- 
-    
-
+    #    self.port               
     def GetConnection():
-        return MySQLdb.connect(host="mysql",    # your host, usually localhost
+        return pymysql.connect(host="mysql",    # your host, usually localhost
                         user="api",                # your username
                         passwd="pass",            # your password
                         db="mysql",                 # name of the data base
                         port=3306)                  # port of the data base 
-           
-     
+
+    def AlterData(query):
+        returnValue = ""
+        if query != "":
+            try:
+                db = pymysql.connect(host="0.0.0.0",    # your host, usually localhost
+                            user="root",              # your username
+                            passwd="pass",          # your password
+                            db="billdb",               # name of the data base
+                            port=33060)                # port of the data base                                
+                cur = db.cursor()            
+                returnValue =  str(cur.execute(query))
+                db.commit()                               
+                print("Rows Changed:{}".format(returnValue))
+            except:
+                returnValue = "-1"
+                print("Error in function: GetmysqlData() please check")                
+            finally:
+                db.close()                    
+        return returnValue
+    
+    def GetmysqlData(query):
+        returnValue = ""
+        if query != "":
+             try:
+                db = pymysql.connect(host="0.0.0.0",    # your host, usually localhost
+                            user="root",              # your username
+                            passwd="pass",          # your password
+                            db="billdb",               # name of the data base
+                            port=33060)                # port of the data base                
+                
+                cur = db.cursor()            
+                cur.execute("SELECT * FROM Provider")                                
+                data = cur.fetchall()                                            
+                returnValue = data
+             except pymysql.err:
+                print("Error in function: GetmysqlData() please check")
+                print(pymysql.err)
+                
+             finally:
+                db.close()
+         
+        return returnValue
+
     def selectOne():
         value = 0
-        try:
-            print("before connect")
-            db = MySQLdb.connect(host="mysql",    # your host, usually localhost
+        try:            
+            db = pymysql.connect(host="0.0.0.0",    # your host, usually localhost
                             user="root",              # your username
                             passwd="pass",          # your password
                             db="mysql",               # name of the data base
-                            port=3306)                # port of the data base    
-            print("after connect")                                        
-            cur = db.cursor()
-            print("after cursor")                                        
-            cur.execute(""" select 1  """)
-            data = cur.fetchall()  
-            print("after fetchall")                                                     
-            value = data[0][0]
-            print(value)
-        
+                            port=33060)                # port of the data base                
+            cur = db.cursor()            
+            cur.execute(""" select 1  """)            
+            data = cur.fetchall()                          
+            value = data[0][0]                    
         except:
             print("Error in function: selectOne() please check")
         finally:
