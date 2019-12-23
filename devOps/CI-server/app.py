343cc0dae9dc0dec5d1a47ca63551b74afb020d9
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-23 09:52:24 +0200

    trying out new webhook

--- devOps/CI-server/app.py
+++ devOps/CI-server/app.py
@@ -1,16 +1,18 @@
-from github_webhook import Webhook
 from flask import Flask
 
 app = Flask(__name__)  # Standard Flask app
-webhook = Webhook(app) # Defines '/postreceive' endpoint
 
-@app.route("/")        # Standard Flask endpoint
+
+@app.route("/", )  # Standard Flask endpoint
 def hello_world():
-    return "Hello, World!"
+    return "CI server is listening..."
 
-@webhook.hook()        # Defines a handler for the 'push' event
-def on_push(data):
+
+@app.route('/webhook', methods=['POST'])
+def webhook(data):
     print("Got push with: {0}".format(data))
+    return "CI server webhooked"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
