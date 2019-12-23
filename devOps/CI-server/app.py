5bbf90bd03b9e21113358ad515142fc4e1df0d0c
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-23 12:18:06 +0200

    CI - creating new git repo

--- devOps/CI-server/app.py
+++ devOps/CI-server/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
+from git import Repo
 import json
+
 app = Flask(__name__)  # Standard Flask app
+
+git_url = "git@github.com:BlueTeam2019/Blue.git"
+repo_dir = "/home/ubuntu/testing"
 
 
 @app.route("/", )  # Standard Flask endpoint
@@ -12,7 +17,12 @@ def hello_world():
 def webhook():
     content = request.json
     print("Got push with: {0}".format(json.dumps(content, indent=2)))
+    create_new_repo(git_url, repo_dir)
     return "CI server webhooked".format(content)
+
+
+def create_new_repo(git_url, repo_dir):
+    Repo.clone_from(git_url, repo_dir)
 
 
 if __name__ == "__main__":
