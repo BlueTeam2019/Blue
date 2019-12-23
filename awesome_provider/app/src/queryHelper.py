578b5fb6962fc162785a532f3fb578e95478787a
Efi <efishimon@gmail.com>
2019-12-23 13:09:46 +0200

    Fixed database connection to work from diffrent containers

--- awesome_provider/app/src/queryHelper.py
+++ awesome_provider/app/src/queryHelper.py
@@ -1,58 +1,42 @@
 import MySQLdb
-class QueryHelper():
-    
-    
-    
-    def __init__(self , url, usr, password, dbName, port):
-        self.url = url
-        self.usr = usr
-        self.password = password
-        self.dbName=dbName
-        self.port
-    
-
-
-   # def __init__(self , url, usr, password, dbName):
-   #     db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-   #                     user="root",         # your username
-   #                     passwd="1",  # your password
-   #                     db="mysql")        # name of the data base
+class QueryHelper():            
+    #def __init__(self , url, usr, password, dbName, port):
+    #    self.url = url
+    #    self.usr = usr
+    #    self.password = password
+    #    self.dbName=dbName
+    #    self.port       
         
     def GetConnection():
-        return MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
-                        user="root",                # your username
-                        passwd="secret",            # your password
+        return MySQLdb.connect(host="mysql",    # your host, usually localhost
+                        user="api",                # your username
+                        passwd="pass",            # your password
                         db="mysql",                 # name of the data base
                         port=3306)                  # port of the data base 
            
-
-   
-        
-   # def __init__(self , url, usr, password, dbName):
-   #     db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-   #                     user="root",         # your username
-   #                     passwd="1",  # your password
-   #                     db="mysql")        # name of the data base
-        
+     
     def selectOne():
         value = 0
         try:
-        db = MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
-                        user="root",              # your username
-                        passwd="secret",          # your password
-                        db="mysql",               # name of the data base
-                        port=3306)                # port of the data base    
-                                 
-        cur = db.cursor()
-        cur.execute(""" select 1  """)
-        data = cur.fetchall()               
+            print("before connect")
+            db = MySQLdb.connect(host="mysql",    # your host, usually localhost
+                            user="root",              # your username
+                            passwd="pass",          # your password
+                            db="mysql",               # name of the data base
+                            port=3306)                # port of the data base    
+            print("after connect")                                        
+            cur = db.cursor()
+            print("after cursor")                                        
+            cur.execute(""" select 1  """)
+            data = cur.fetchall()  
+            print("after fetchall")                                                     
             value = data[0][0]
             print(value)
         
         except:
             print("Error in function: selectOne() please check")
         finally:
-        db.close()
+            db.close()
                     
         return int(value)
 
