9e74be1809d25214903cd80e838aa28fd45061f1
hannah <hannah@gmail>
2019-12-24 13:04:26 +0200

    create put & post

--- awesome_provider/app/src/provider_model.py
+++ awesome_provider/app/src/provider_model.py
@@ -1,5 +1,5 @@
 import queryHelper
-
+import json
 class provider_model:
     #def __init__(self , url, usr, password, dbName):
      #   self.query = QueryHelper(url, usr, password, dbName)
@@ -16,5 +16,29 @@ class provider_model:
         return isAlive
     
     
+    def createProvider(name):
+        isNameExist=0
+        #TO-DO:check if there is another way
+        isNameExist="select Count(*) from Provider where name='{}';".format(name)
+        if isNameExist == 0: 
+            sql="INSERT INTO Provider (`name`) VALUES ('{}');".format(name)
+            getId="select id from Provider where name='{}';".format(name)
+            data={'id': getId }
+            data=json.dumps(data)
+        result=json.dumps(data)
+        print(result)
+        return result
+
+
+    def updateProvider(id ,name):
+        isNameExist="select Count(*) from Provider where id='{}';".format(id)
+        if isNameExist != 0: 
+            sql = "UPDATE Provider SET  name='{}' where id ='{}';".format(name,id)
+            return 1
+        return 0
         
-        
+# SELECT COUNT(*)
+# FROM information_schema.statistics
+# WHERE TABLE_SCHEMA = DATABASE()
+#   AND TABLE_NAME = 'Provider' 
+#   AND INDEX_NAME = 'name'; 
--- awesome_provider/app/src/provider_routing.py
+++ awesome_provider/app/src/provider_routing.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import provider_model
+from provider_model import provider_model
 import sys
 
 app = Flask(__name__)
@@ -8,11 +8,34 @@ app = Flask(__name__)
 @app.route('/health', methods=["GET"])
 def CheckHealth():
     print("in route")
-    isAlive = provider_model.provider_model.CheckHealth()
+    isAlive = provider_model.CheckHealth()
     if isAlive:
         return "OK", 200
     else:
-        return "Internal Error", 500 
+        return "Internal Error", 500
+
+@app.route('/provider', methods=["POST"])
+def PostProvider():
+    data= request.json
+    result=provider_model.createProvider(data["name"])
+    if result == 0 :
+        return "The name is exist ,Choose another one", 404
+    return f"{data['name']}"
 
 
-app.run(host='0.0.0.0')
+@app.route('/provider/<int:id>', methods=["PUT"])
+def PutProvider(id):
+    print("YOHO")
+    data= request.json
+    result=provider_model.updateProvider(id,data["name"])
+    if result == 0 :
+        return "Wrong id", 404
+    return "put ok" ,200
+
+# @app.route('/put/<int:id>', methods=["PUT"])
+# def Check(id):
+#     return "put" ,200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=8080)          
