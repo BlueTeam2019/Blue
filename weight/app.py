87a8e78ba171ea282d6b46a7256380c0b6e6f368
I C <labthyneighbor@gmail.com>
2019-12-24 15:19:46 +0200

    app.py changed to pymysql; batch-weight up and working

--- weight/app.py
+++ weight/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 import os
-import mysql.connector
+import pymysql
 import pandas as pd
 from flask import Flask, request
 from sqlalchemy import create_engine
 from werkzeug.utils import secure_filename
 
-# APP_PORT    = 5000
+ACCESS_PORT = 5000
 IN_DIR      = 'in/' # Path to Blue/weight/in folder (batch-weight)
 
 MYSQL_DB	= 'weight'
 MYSQL_USER  = 'root'
 MYSQL_PW 	= 'pass'
-MYSQL_HOST 	= 'mysql'
+MYSQL_HOST 	= '0.0.0.0'
 MYSQL_PORT	= '3306'
 
 app = Flask(__name__)
@@ -22,31 +22,23 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 @app.route("/health", methods=['GET'])
 def checkalive():
-    alive = True
-    config = {
-        'user': MYSQL_USER,
-        'password': MYSQL_PW,
-        'host': MYSQL_HOST,
-        'port': MYSQL_PORT,
-        'database': MYSQL_DB,
-        'raise_on_warnings': True
-        }      # using mysql.connector
-    cnx = mysql.connector.connect(**config)
-    cursor = cnx.cursor()
-
-    if cursor.execute(""" select * from containers_registered """):
-        alive = False   #  FALSE if SQL not reached
+    try:
+        db = pymysql.connect(host=MYSQL_HOST,user=MYSQL_USER,passwd=MYSQL_PW,db=MYSQL_DB)
+    except Exception:
+        return "Error in MySQL connexion", 500
     else:
-        print("alive is: ", alive)
-        for (id, weight, unit) in cursor:
-            print(id, weight, unit)
-    cursor.close()
-
-    cnx.close()
-    if alive:
-        return "HOME is where ♥Heart is❤❤❤", 200
-    else:
-        return "This is UN-Healthy", 500
+        cur = db.cursor()
+        try:
+            query=""" select * from containers_registered; """
+            cur.execute(query)
+        except Exception:
+            return "Error with query: " + query, 500
+        else:
+            db.commit()
+            result = cur.fetchall()
+            print(result)
+        db.close() 
+    return "HOME is where ♥Heart is❤❤❤", 200
 
 
 @app.route("/batch-weight", methods=['POST'])
@@ -56,25 +48,22 @@ def batch_up():
     if not filename:    #--if file was uploaded, then save it to /in
         f = request.files['file']
         if not f:
-            return "NO FILE was mentioned or uploaded"
+            return "NO FILE was POSTed"
         filename = secure_filename(f.filename)
         f.save(os.path.join(IN_DIR, filename))
-
-    suffix=filename.split('.')[-1].lower()
-
     # 2- check if csv or json, to parse correctly
+    suffix=filename.split('.')[-1].lower()
     if suffix=='csv':
         df = pd.read_csv(IN_DIR+filename)
     elif suffix=='json':
         df = pd.read_json(IN_DIR+filename)
-
     # 3- maintain a connection to MySQL DB
     engine = create_engine('mysql+mysqlconnector://'+MYSQL_USER+':'\
         +MYSQL_PW+'@'+MYSQL_HOST+':'+MYSQL_PORT+'/'+MYSQL_DB, echo=False)
-
     # 4- send query (INSERT), replacing current table values
     with engine.connect() as conn, conn.begin():
         df.to_sql('containers_registered', conn, if_exists='replace')
     return "fine"
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+
+app.run(host="0.0.0.0", port=ACCESS_PORT, debug=True)
