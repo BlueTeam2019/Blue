5773d16ddc19f1b54a55fa93b164646a22c9a606
develeap <roy.barak.m@gmail.com>
2019-12-25 14:31:12 +0200

    -add-weight-to-model

--- awesome_provider/app/src/model.py
+++ awesome_provider/app/src/model.py
@@ -21,3 +21,8 @@ class Model(object):
     def get_rate(self, products, id):
         return 0
 
+    def get_weights(self, from_time, to_time):
+        # issue: pass as parameters ?from_t=<int>&to_t=<int>&f="out"
+        response = requests.get(self.weight_url, from_t, to_t, "out")
+        return json.loads(response)
+
