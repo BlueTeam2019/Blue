546aaa6905f4d978396764f17b4ff19c0c6da967
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-25 18:13:48 +0200

    playing with colors

--- devOps/CI-server/app.py
+++ devOps/CI-server/app.py
@@ -65,7 +65,7 @@ def webhook():
 
     # creating a local repository for testing
     print("\n\n")
-    cprint('Cloning repository...', 'red', 'on_white', attrs=['bold'])
+    cprint('Cloning repository for {0}...'.format(head_commit), 'red', 'on_white', attrs=['bold'])
     repo = repo_dir + head_commit
     create_repo_of_commit(git_url, repo, head_commit)
 
@@ -76,16 +76,29 @@ def webhook():
     test_version_hash = "Test server: " + branch_name + " - " + head_commit
 
     # testing build and sending reports
+    print("\n\n")
+    cprint('Executing tests...', 'red', 'on_white', attrs=['bold'])
     test_passed, results = exec_tests(repo + providor_run_tests_path, repo + weight_run_tests_path)
+    if test_passed:
+        cprint('All tests passed! sending report...', 'green', attrs=['bold'])
+    else:
+        cprint('Some tests failed! will not update production. sending report...', 'red', attrs=['bold'])
     sendReport.send_report(test_passed, results, pusher_email)
 
     # if the test passed - push to production
     if branch_name == "master" and test_passed:
+        print("\n\n")
+        cprint('Updating production...', 'red', 'on_white', attrs=['bold'])
         build(repo + weight_path_prod, repo + providor_path_prod)
         shutil.move(repo, master_history_path, copy_function=shutil.copytree)
         version_hash = "Production server: " + branch_name + " - " + head_commit
 
+    print("\n\n")
+    cprint('Cleaning up environment...', 'red', 'on_white', attrs=['bold'])
     clean_env()
+
+    print("\n\n")
+    print(colored('DONE', 'red', attrs=['reverse', 'blink']))
     return "CI server webhooked".format(content)
 
 
