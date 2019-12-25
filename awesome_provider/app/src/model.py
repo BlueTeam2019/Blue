665cb0df614498996fdfa85304c79d45940d78c7
hannah <hannah@gmail>
2019-12-25 11:24:33 +0200

    Unify Model interface for /providers

--- awesome_provider/app/src/model.py
+++ awesome_provider/app/src/model.py
@@ -1,69 +1,6 @@
-<<<<<<< HEAD
-import queryHelper
+
 import json
-class Model(object):
-    #def __init__(self , url, usr, password, dbName):
-     #   self.query = QueryHelper(url, usr, password, dbName)
- def __init__(self, query_helper):
-        self.query = query_helper
 
-    def check_health(self):
-        data = self.query.select_one()
-        if int(data[0][0]) == 1:
-            return True
-        return False
-
-
-
-    
-    
-    def isColumnInProviderExist(self,column,name):
-        isExist=0
-        isExist=queryHelper.QueryHelper.GetData("select Count(*) from Provider where {}='{}';".format(column,name))
-        isExist=isExist[0][0]
-        print(isExist)
-        if isExist == 0: 
-            return 0
-        return 1   
-
-
-    def createProvider(name):
-        isNameExist=provider_model.isColumnInProviderExist("name",name)
-        result=0
-        if isNameExist == 0: 
-            print("name is not exist")
-            sql=queryHelper.QueryHelper.GetData("INSERT INTO Provider (`name`) VALUES ('{}');".format(name))
-            getId=queryHelper.QueryHelper.GetData("select id from Provider where name='{}';".format(name))
-            data={'id': getId[0][0] }
-            result=json.dumps(data)
-        print(result)
-        return result
-
-
-    def updateProvider(self,id ,name):
-        isIdExist=-1
-        isIdExist=isColumnInProviderExist("id",id)
-        #all=queryHelper.QueryHelper.GetData("select * from Provider;")
-        #print(all)
-        #isIdExist=isIdExist[0][0]
-        print(isIdExist)
-        if isIdExist != 0: 
-            isNameExist=provider_model.isColumnInProviderExist("name",name)
-            if isNameExist == 0:
-                sql = "UPDATE Provider SET  name='{}' where id ={};".format(name,id)
-                return "Update succedded" ,200
-            return "The name is exist" ,404
-            #isIdExist=queryHelper.QueryHelper.AlterData(sql)
-            #print(isIdExist)
-            
-        return  "The id is not exist" ,404
-        
-# SELECT COUNT(*)
-# FROM information_schema.statistics
-# WHERE TABLE_SCHEMA = DATABASE()
-#   AND TABLE_NAME = 'Provider' 
-#   AND INDEX_NAME = 'name'; 
-=======
 
 class Model(object):
     def __init__(self, query_helper):
@@ -74,4 +11,40 @@ class Model(object):
         if int(data[0][0]) == 1:
             return True
         return False
->>>>>>> ccadbb36047d4ae5581542c5b701ccd6b0bd8684
+    
+    def is_column_in_provider_exist(self, column, name):
+        is_exist = self.query.get_data(f"select Count(*) from Provider where {column}='{name}';")
+        is_exist = is_exist[0][0]
+        print(is_exist)
+        if not is_exist:
+            return False
+        return True
+
+    def create_provider(self, name):
+        """
+        Receives a Provider name, checks existence of name then inserts into DB
+        :param name: Provider name
+        :return:
+        """
+        is_name_exist = self.is_column_in_provider_exist("name", name)
+        result = ''
+        if not is_name_exist:
+            print("name is not exist")
+            self.query.get_data(f"INSERT INTO Provider (`name`) VALUES ('{name}');")
+            get_id = self.query.get_data(f"select id from Provider where name='{name}';")
+            result = json.dumps({'id': get_id[0][0]})
+        print(result)
+        return result
+
+    def update_provider(self, id, name):
+        is_id_exist = self.is_column_in_provider_exist("id", id)
+        print(is_id_exist)
+        if is_id_exist:
+            is_name_exist = self.is_column_in_provider_exist("name", name)
+            if not is_name_exist:
+                sql_query = f"UPDATE Provider SET  name='{name}' where id ={id};"
+                self.query.alter_data(sql_query)
+                return True, "Update succeeded"
+            return False, "The name is exist"
+
+        return False, "The id is not exist"
