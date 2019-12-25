43287b34d07489e1f38ee7f96e30da2cb13998b2
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-25 15:58:25 +0200

    colors to CI

--- devOps/CI-server/app.py
+++ devOps/CI-server/app.py
@@ -22,6 +22,15 @@ master_history_path = "/home/ubuntu/master_hist"
 version_hash = "production is down."
 test_version_hash = "testing is down"
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # Standard Flask endpoint
 @app.route("/", )
@@ -34,6 +43,8 @@ def hello_world():
 # webhook to github
 @app.route('/webhook', methods=['POST'])
 def webhook():
+
+    print(f"{bcolors.WARNING}Warning: No active frommets remain. Continue?{bcolors.ENDC}")
     global version_hash
     global test_version_hash
     # trying to free up space
