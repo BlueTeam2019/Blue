74654cebc6843ae84d0be6cdc4cb69002aa3f068
Assi Hamza <33433289+asi91@users.noreply.github.com>
2019-12-25 12:37:27 +0200

    Feature/provider and get truck api (#16)
    * Unify Model interface for /providers
    * Implement /provider routes
    	POST - Inserts new provider by name into DB
    	PUT  - Update provider's name by ID
    * Implement GET /truck - relies on /item endpoint from Weight API

--- awesome_provider/app/src/model.py
+++ awesome_provider/app/src/model.py
@@ -1,4 +1,7 @@
 
+import json
+
+
 class Model(object):
     def __init__(self, query_helper):
         self.query = query_helper
@@ -8,3 +11,40 @@ class Model(object):
         if int(data[0][0]) == 1:
             return True
         return False
+    
+    def is_column_in_provider_exist(self, column, name):
+        is_exist = self.query.get_data(f"select Count(*) from Provider where {column}='{name}';")
+        is_exist = is_exist[0][0]
+        print(is_exist)
+        if not is_exist:
+            return False
+        return True
+
+    def create_provider(self, name):
+        """
+        Receives a Provider name, checks existence of name then inserts into DB
+        :param name: Provider name
+        :return:
+        """
+        is_name_exist = self.is_column_in_provider_exist("name", name)
+        result = ''
+        if not is_name_exist:
+            print("name is not exist")
+            self.query.get_data(f"INSERT INTO Provider (`name`) VALUES ('{name}');")
+            get_id = self.query.get_data(f"select id from Provider where name='{name}';")
+            result = json.dumps({'id': get_id[0][0]})
+        print(result)
+        return result
+
+    def update_provider(self, id, name):
+        is_id_exist = self.is_column_in_provider_exist("id", id)
+        print(is_id_exist)
+        if is_id_exist:
+            is_name_exist = self.is_column_in_provider_exist("name", name)
+            if not is_name_exist:
+                sql_query = f"UPDATE Provider SET  name='{name}' where id ={id};"
+                self.query.alter_data(sql_query)
+                return True, "Update succeeded"
+            return False, "The name is exist"
+
+        return False, "The id is not exist"
--- awesome_provider/app/src/routing.py
+++ awesome_provider/app/src/routing.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, request
+import datetime
+
 from model import Model
-
-
 from query_helper import QueryHelper
+
 
 app = Flask(__name__)
 
@@ -13,6 +14,50 @@ def check_health():
     if model.check_health():
         return "OK", 200
     return "Internal Error", 500
+
+
+@app.route('/provider', methods=["POST"])
+def post_provider():
+    data = request.json
+    result = model.create_provider(data["name"])
+    if not result:
+        return "The name is exist ,Choose another one", 404
+    return result, 200
+
+
+@app.route('/provider/<int:id>', methods=["PUT"])
+def put_provider(id):
+    data = request.json
+    result = model.update_provider(id, data["name"])
+    if not result[0]:
+        return result[1], 404
+    return result[1], 200
+
+
+@app.route('/truck/<int:id>/', methods=["GET"])
+def get_truck(id):
+    time_from = request.args["from"]
+    time_to = request.args["to"]
+    valid_time_from_format = validate_time_format(time_from)
+    valid_time_to_format = validate_time_format(time_to)
+    if not valid_time_from_format[0]:
+        return f"from: {valid_time_from_format[1]}", 404
+    if not valid_time_to_format[0]:
+        return f"To:{valid_time_to_format[1]}", 404
+
+    # return request.get(f"localhost:8082/item/{id}','from':{time_from} ,'to':{time_to}")
+    print("ok")
+    return "ok", 200
+
+
+def validate_time_format(time_to_validate):
+    date_format = '%Y%m%d%H%M%S'
+    try:
+        date_obj = datetime.datetime.strptime(time_to_validate, date_format)
+        print(date_obj)
+        return True, "Succeeded"
+    except ValueError:
+        return False, "Incorrect data format, should be YYYYMMDDHHMMSS"
 
 
 if __name__ == '__main__':
@@ -25,4 +70,4 @@ if __name__ == '__main__':
     query_helper = QueryHelper(db_url, db_user, db_pass, db_name, db_port)
     model = Model(query_helper)
 
-    app.run(host='0.0.0.0', debug=bool(do_debug))
+    app.run(host='0.0.0.0', debug=bool(do_debug))  
