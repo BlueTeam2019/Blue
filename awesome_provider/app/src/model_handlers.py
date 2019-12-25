e7aa4868023422b14b69da05b40f6025a74ebc50
Assi Hamza <33433289+asi91@users.noreply.github.com>
2019-12-25 14:32:32 +0200

    Feature/rates api (#17)
    * Handle excel file manipulation
    
    * Overwrite POST /rates excel data into Rates table and save file in app/in directory
    
    * Implement /rate endpoint
    	POST Overwrites data into Rates table
    	GET returns a copy of the latest values as an excel file

---
+++ awesome_provider/app/src/model_handlers.py
@@ -0,0 +1,23 @@
+import pandas
+
+
+def generate_query_from_excel(file):
+    """
+    Receives an excel file, parses its content into a SQL query, then saves the file to ../in folder
+
+    file: Excel file
+    return: SQL VALUES query
+    """
+    data = pandas.read_excel(file)
+    product, rate, scope = list(data.columns)
+    query_values = []
+
+    # Iterate over the file contents and build SQL VALUES query
+    for idx, row in data.iterrows():
+        query_values.append(f"('{row[product]}','{row[rate]}','{row[scope]}')")
+
+    # Save and overwrite the new excel file
+    data.to_excel('../in/rates.xlsx', "rates", index=False)
+
+    # Return properly formatted query
+    return ',\n'.join(query_values) + ';'
