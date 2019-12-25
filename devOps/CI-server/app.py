dfdcee7cb1e86327501e75a41478ff7420c98281
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-25 11:37:02 +0200

    CI renove orphans

--- devOps/CI-server/app.py
+++ devOps/CI-server/app.py
@@ -81,9 +81,9 @@ def clean_env():
 
 def build(weight_path, provider_path):
     subprocess.run("docker-compose -f {0} build --no-cache".format(weight_path), shell=True)
-    subprocess.run("docker-compose -f {0} up -d ".format(weight_path), shell=True)
+    subprocess.run("docker-compose -f {0} up -d --remove-orphans".format(weight_path), shell=True)
     subprocess.run("docker-compose -f {0} build --no-cache".format(provider_path), shell=True)
-    subprocess.run("docker-compose -f {0} up -d ".format(provider_path), shell=True)
+    subprocess.run("docker-compose -f {0} up -d --remove-orphans".format(provider_path), shell=True)
 
 
 if __name__ == "__main__":
