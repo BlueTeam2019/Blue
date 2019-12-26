b9ed908e6342e00c1362aa1140f9f1cae2b2d7d2
udik7 <udi_k7@walla.co.il>
2019-12-26 14:31:40 +0200

    get /ITEMs; removed all other functions from app.py; commented out datetime for local var errors

--- weight/app.py
+++ weight/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
 import os
 import pymysql
 import pandas as pd
-from flask import jsonify
-from flask import Flask, request
+from datetime import datetime
+from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
 from werkzeug.utils import secure_filename
 
@@ -23,78 +22,45 @@ def getMysqlConnection():
     return pymysql.connect(host=MYSQL_HOST,user=MYSQL_USER,passwd=MYSQL_PW,\
         db=MYSQL_DB,cursorclass=pymysql.cursors.DictCursor)
 
-@app.route("/", methods=['GET'])
-@app.route("/health", methods=['GET'])
-def checkalive():
-    try:
-        db = getMysqlConnection()
-    except Exception:
-        return "Error in MySQL connexion", 500
-    else:
-        cur = db.cursor()
-        try:
-            query=""" select * from containers_registered; """
-            cur.execute(query)
-        except Exception:
-            return "Error with query: " + query, 500
-        else:
-            db.commit()
-            result = cur.fetchall()
-            print(result)
-        db.close() 
-    return "HOME is where ♥Heart is❤❤❤", 200
 
-
-@app.route("/batch-weight", methods=['POST'])
-def batch_up():
-    # 1- receive file (to be parsed) or filename (to be looked in /in)
-    filename=request.form.get('file')
-    if not filename:    #--if file was uploaded, then save it to /in
-        f = request.files['file']
-        if not f:
-            return "NO FILE was POSTed"
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(IN_DIR, filename))
-    # 2- check if csv or json, to parse correctly
-    suffix=filename.split('.')[-1].lower()
-    if suffix=='csv':
-        df = pd.read_csv(IN_DIR+filename)
-    elif suffix=='json':
-        df = pd.read_json(IN_DIR+filename)
-    # 3- maintain a connection to MySQL DB
-    engine = create_engine('mysql+pymysql://'+MYSQL_USER+':'\
-        +MYSQL_PW+'@'+MYSQL_HOST+':'+MYSQL_PORT+'/'+MYSQL_DB, echo=False)
-    # 4- send query (INSERT), replacing current table values
-    with engine.connect() as conn, conn.begin():
-        df.to_sql('containers_registered', conn, if_exists='replace')
-    return "fine"
-
-
-@app.route('/session/<id>', methods=['GET'])
-def session(id):
-    db = getMysqlConnection() # CHANGE
-    
-                #   origional
-                #       getsession = "SELECT JSON_ARRAYAGG(JSON_OBJECT('id', id, 'created', created_at, 'truckid', truckid, 'Bruto', bruto, 'truckTara', truckTara, 'Neto', neto)) from sessions where id='%s'" %id
-    getsession = "SELECT * from transactions where id={}".format(id)
-
-        #return id',truckid', 'Bruto')
-        #IF OUT  (truckTara, neto)  
-
+@app.route('/item/<string:id_num>', methods=['GET'])
+def get_item_id(id_num):
+    db = getMysqlConnection()
     cur = db.cursor()
-    cur.execute(getsession)
-    output = cur.fetchone()
-    db.close()
-    if output==None:
-        return jsonify({'message':'No session found', 'status':404})
 
-    return_json=jsonify({"id":output['id'],"truck":output['truck'],"bruto":output['bruto']})
-        #ONLY for OUT:
-    if output['direction']=="out":
-        return_json=jsonify({"id":output['id'],"truck":output['truck'],"bruto":output['bruto'],"truckTara":output['truckTara'],"neto":output['neto']})
+    # from_t1 = request.args.get('from')
+    # if not from_t1:
+    #      from_t1 = datetime.now().strftime('%Y%m"+"01000000')
+    # to_t2 = request.args.get('to')
+    # if not to_t2:
+    #     to_t2 = datetime.now().strftime('%Y%m%d%H%M%S')
 
-    return return_json
-
-
+    try:
+        id_query = ("SELECT * FROM containers_registered WHERE container_id=" + "'" + id_num + "'")
+        cur.execute(id_query)
+        row = cur.fetchone()
+        if row:
+            json_data = {'id': id_num, 'tara': row['weight'] , 'sessions': [] }
+            return jsonify(json_data)
+        else:
+            id_query = ("SELECT * FROM transactions WHERE truck = " + "'" + id_num + "';")
+            cur.execute(id_query)
+            data = cur.fetchall()
+            if data:
+                ret_id = []
+                for line in data:
+                    id, datetime, direction, truck, containers, bruto, truckTara, neto, produce = line
+                    last_weight = line['truckTara']
+                    ret_id.append(line['id'])
+                if not last_weight:
+                    last_weight = "N/A"
+                json_data = {'id': id_num , 'tara': last_weight , 'sessions' : ret_id }
+                return jsonify(json_data)
+            else:
+                return jsonify("no session found")
+    except Exception as e:
+        return jsonify("EXCEPTION it is", str(e))
+    finally:
+        db.close()
 
 app.run(host="0.0.0.0", port=ACCESS_PORT, debug=True)
